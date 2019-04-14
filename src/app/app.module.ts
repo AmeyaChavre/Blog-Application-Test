d41d65d3cb645332f47d8537032b31396748671f
Your Name <you@example.com>
2019-04-14 23:30:37 +0530

    Commit#1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent,
+    BlogCreateComponent,
+    BlogViewComponent,
+    BlogEditComponent
   ],
   imports: [
     BrowserModule
