5d5462e06371854fded9a6a9e4e9ccbf68d0c7ed
Your Name <you@example.com>
2019-04-16 22:15:33 +0530

    Commit#4

--- src/app/blog-view/blog-view.component.ts
+++ src/app/blog-view/blog-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-blog-view',
@@ -7,9 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogViewComponent implements OnInit {
 
-  constructor() { }
+  public currentBlog;
+
+  public allBlogs = [
+    {
+      "blogId": "1",
+      "lastModified": "2019-01-20T12:20:47.854Z",
+      "created": "2019-01-20T12:20:47.854Z",
+      "tags": ["Prison","Tim Robbins"],
+      "author": "Admin",
+      "category": "Prison Drama",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml": "This is blog 1 body",
+      "description": "This is blog 1 description",
+      "title": "This is blog 1"
+    },
+    {
+      "blogId": "2",
+      "lastModified": "2019-01-20T12:20:47.854Z",
+      "created": "2019-01-20T12:20:47.854Z",
+      "tags": [],
+      "author": "Admin",
+      "category": "Prison Drama",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml": "This is blog 2 body",
+      "description": "This is blog 1 description",
+      "title": "This is blog 2"
+    },
+    {
+      "blogId": "3",
+      "lastModified": "2019-01-20T12:20:47.854Z",
+      "created": "2019-01-20T12:20:47.854Z",
+      "tags": [],
+      "author": "Admin",
+      "category": "Prison Drama",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml": "This is blog 3 body",
+      "description": "This is blog 3 description",
+      "title": "This is blog 3"
+    }
+  ]
+
+  constructor(private _route: ActivatedRoute, private router: Router) {
+    console.log("Constructor is called.");
+  }
 
   ngOnInit() {
+    console.log("ngOnInit() is called.");
+    let myBlogId = this._route.snapshot.paramMap.get('blogId');
+    console.log(myBlogId);
+    this.getSingleBlogInformation(myBlogId);
+  }
+
+  public getSingleBlogInformation(currentBlogId): any {
+
+    for (let blog of this.allBlogs) {
+      if (blog.blogId == currentBlogId) {
+        this.currentBlog = blog;
+      }
+    }
+    console.log(this.currentBlog);
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public allBlogs = [
+    {
+      "blogId": "1",
+      "lastModified": "2019-01-20T12:20:47.854Z",
+      "created": "2019-01-20T12:20:47.854Z",
+      "tags": ["Prison","Tim Robbins"],
+      "author": "Admin",
+      "category": "Prison Drama",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml": "This is blog 1 body",
+      "description": "This is blog 1 description",
+      "title": "This is blog 1"
+    },
+    {
+      "blogId": "2",
+      "lastModified": "2019-01-20T12:20:47.854Z",
+      "created": "2019-01-20T12:20:47.854Z",
+      "tags": [],
+      "author": "Admin",
+      "category": "Prison Drama",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml": "This is blog 2 body",
+      "description": "This is blog 1 description",
+      "title": "This is blog 2"
+    },
+    {
+      "blogId": "3",
+      "lastModified": "2019-01-20T12:20:47.854Z",
+      "created": "2019-01-20T12:20:47.854Z",
+      "tags": [],
+      "author": "Admin",
+      "category": "Prison Drama",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml": "This is blog 3 body",
+      "description": "This is blog 3 description",
+      "title": "This is blog 3"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
